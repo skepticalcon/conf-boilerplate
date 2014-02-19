7a18961c05d5bf0556ccc121fde51d8df29ecfd3
Tim Gerla <tim@gerla.net>
2014-02-18 21:56:51 -0600

    big updates

--- docpad.js
+++ docpad.js
@@ -33,8 +33,8 @@ module.exports = {
     // Site info
     site: {
       theme: "skeptical",
-      url: "http://braziljs.github.io/conf-boilerplate/",
-      googleanalytics: "UA-33656081-1"
+      url: "http://www.skepticalcon.com",
+      googleanalytics: "UA-28552149-1"
     },
 
     // Active sections on the website
@@ -44,10 +44,8 @@ module.exports = {
       'about',
       'location',
       'speakers',
-      'schedule',
-      'sponsors',
-      'partners'
-      // 'contact'
+//      'schedule',
+      'contact'
     ],
 
     // Labels which you can translate to other languages
@@ -63,19 +61,19 @@ module.exports = {
 
     // The entire schedule
     schedule: [
-      {
-        name: "Check-in / Breakfast",
-        time: "9h00"
-      },
+//      {
+//        name: "Check-in / Breakfast",
+//        time: "9h00"
+//      },
       {
         name: "Sheldon Helms",
         photo: "themes/skeptical/img/speakers/helms.jpg",
         bio: "Sheldon W. Helms is an associate professor of psychology at Ohlone College in Fremont, CA.  He serves on the Board of Directors for the Bay Area Skeptics, is a member of the SkeptiCal planning committee, and is the founder of the Ohlone Psychology Club Speaker Series through which he regularly hosts top name speakers in science and skepticism.",
         company: "Ohlone College, Fremont, California",
         presentation: {
-          title: "Digging into a Linux Kernel",
+          title: "Psychological Pseudoscience",
           description: "As doctors battle homeopathy and anti-vaccine myths, anthropologists counter creationist claims, and dietitians deal with the unwarranted battle against gluten and GMOs, much of the skeptical community is oddly silent on dubious claims from fringe psychology. In this talk, psychology professor Sheldon W. Helms will discuss unsupported and sometimes dangerously pseudo-scientific claims from the field of psychology, giving a brief history and explanation of each practice or theory, and demonstrating how a simple application of the scientific method reveals its flaws.",
-          time: "10h00"
+ //         time: "10h00"
         }
       },
       {
@@ -87,13 +85,13 @@ module.exports = {
         presentation: {
           title: "Pseudohistory Hurts!",
           description: "In this hour we'll take a dozen cases of false pseudohistory that are commonly believed or promoted through popular media, and study why they are harmful to society. More importantly, we'll discover the true benefits and lessons that come from knowing what really happened.",
-          time: "11h00"
+//          time: "11h00"
         }
       },
-      {
-        name: "Lunch",
-        time: "12h00"
-      },
+//      {
+//        name: "Lunch",
+//        time: "12h00"
+//      },
       {
         name: "Paul Doherty",
         photo: "themes/skeptical/img/speakers/doherty.jpg",
@@ -103,54 +101,35 @@ module.exports = {
         presentation: {
           title: "The Boundaries of Science",
           description: "If you plot every object we know in the universe on a graph of mass versus radius all of the objects appear within a triangle of three lines: the event horizon of a black hole, the radius of the visible universe and the compton wavelength. Inside this triangle we can do experimental science. Outside of this triangle we can theorize but cannot test our theories. Scientists always push on the boundaries and as we make progress pushing experiments into new realms we expand the boundaries of science.",
-          time: "13h00"
+ //         time: "13h00"
         }
       },
       {
         name: "Dan Dugan",
         photo: "themes/skeptical/img/speakers/dugan.jpg",
         bio: "Dan Dugan is well-known in audio engineering as the inventor of the automatic microphone mixer. His patented equipment is used on many popular television productions including PBS News Hour, the presidential debates, and ESPN sports. In addition to engineering, Dan has a lively interest in philosophy, particularly skepticism, the philosophy of science, and current controversies about scientific paradigms and alternative medicine. He is the Secretary of PLANS (People for Legal and Nonsectarian Schools, Inc.), a whistle-blowing organization opposing taxpayer funding of Waldorf education.",
-        company: "",
+        company: "People for Legal and Nonsectarian Schools, Inc.",
         presentation: {
-          title: "Waldorf",
-          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-          time: "14h00"
+          title: "Thirteen Years to Failure: A Federal Lawsuit to Stop Public Funding of Cult-Like Waldorf Schools",
+          description: "In 1996, the opening of the Internet to the general population brought together a diverse  group of former Waldorf school parents, teachers, and board members who all had experiences with Anthroposophy, the world-wide organization behind Waldorf schools (called Steiner schools outside the U.S.). Their experiences were best explained if Anthroposophy were categorized as a religious cult. At the same time, Waldorf schools began to receive public funding as magnet and charter schools. PLANS was incorporated as a non-profit that could bring legal action against perceived violations of the Establishment Clause of the U.S. Constitution, which forbids public funding of religious activities. A lawsuit was filed in federal court against two California school districts, one with a Waldorf magnet school and one with a Waldorf charter school. First Amendment lawsuits take many years, and this was no exception. Volunteer lawyers came and went. Mistakes were made that the insurance-funded lawyers for the school districts exploited to whittle down the evidence. Despite three favorable rulings from the 9th Circuit Court of Appeals over the years, the case was dismissed in 2012 after the appeals court opined that there was no significant evidence left. The dismissal was without prejudice, however, and the way was left open to start all over again.",
+//          time: "14h00"
         }
       },
-      {
+ /*     {
         name: "Coffee-break",
         time: "15h00"
-      },
+      },*/
       {
-        name: "Mark Zuckerberg",
-        photo: "themes/yellow-swan/img/speaker.jpg",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        company: "Facebook",
-        link: {
-          href: "http://twitter.com/zuck",
-          text: "@zuck"
-        },
+        name: "Patrick O'Reilly",
+        photo: "themes/skeptical/img/speakers/oreilly.jpg",
+        bio: "Patrick O'Reilly is a clinical psychologist and an assistant clinical professor of psychiatry at the University of California, San Francisco School of Medicine. He is Past President of the San Francisco Psychological Association and former Chair of Bay Area Skeptics.",
+        company: "University of California, San Francisco School of Medicine",
         presentation: {
-          title: "Revealing Facebook Secrets",
-          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-          time: "16h00"
+          title: "Cons, Scams and Undue Influence",
+          description: "Internet Scams,  Ponzi schemes, real estate rip-offs, weird cults, fortune-telling cons – it’s hard to read the news without finding another successful con artist at work. As the number of scams and victims grows, the term “undue influence” is gaining widespread use. Dr. O’Reilly will explain the vulnerabilities that these scammers look for and techniques they use to steal from us.",
+//          time: "16h00"
         }
       },
-      {
-        name: "Steve Wozniak",
-        photo: "themes/yellow-swan/img/speaker.jpg",
-        bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-        company: "Apple, Inc.",
-        link: {
-          href: "http://twitter.com/woz",
-          text: "@woz"
-        },
-        presentation: {
-          title: "Why do I prefer Android over iPhone",
-          description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo",
-          time: "17h00"
-        }
-      }
     ],
 
     // List of Sponsors
